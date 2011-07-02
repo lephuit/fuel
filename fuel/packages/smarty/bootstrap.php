d5dabdf20175e936932b2adb45317edc6c298a1b
Adam Balogh <baloghbh@gmail.com>
2011-07-02 03:09:11 +0200

    smarty package

---
+++ fuel/packages/smarty/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     b3ha
+ * @license    MIT License
+ * @copyright  2011 b3ha
+ * @link       http://fuelphp.com
+ */
+
+
+Autoloader::add_classes(array(
+	'Smarty' => __DIR__.'/libs/Smarty.class.php'
+));
+
+
+/* End of file bootstrap.php */
